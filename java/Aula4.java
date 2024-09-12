8fafc71724507c14d8adc54a78b0541473992ca1
Geneblis <94307472+Geneblis@users.noreply.github.com>
2024-09-12 11:45:22 -0300

    33

---
+++ java/Aula4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Aula4 {
+    public static void main(String[] args) {
+        int contador = 8;
+        int numeroaleatorio1 = 3;
+        int numeroaleatorio2 = 7;   
+
+        while (contador != 0) {
+            numeroaleatorio1 = numeroaleatorio1 * numeroaleatorio2;
+            numeroaleatorio2 = numeroaleatorio2 * numeroaleatorio1;
+            System.out.println(numeroaleatorio1);
+            System.out.println(numeroaleatorio2);
+            System.out.println("Coloque 0 para finalizar o programa.");
+            Scanner teclado = new Scanner(System.in);
+            contador = teclado.nextInt();
+        }
+    }
+}
