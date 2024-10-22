35ab0648befcc1248e218cf700ff8eecfb87a36e
Geneblis <94307472+Geneblis@users.noreply.github.com>
2024-10-22 14:11:07 +0000

    hugy

---
+++ html/javascript/test.js
@@ -0,0 +1,17 @@
+alert("ola!")
+let int = 3;
+let cagadas = "Cagadas";
+let peso = 0.3;
+
+function somar(a) {
+    return a + int
+}
+
+let resultado = somar(20);
+
+function subtrair(b) {
+    let valor_temp = 10
+    return a - valor_temp
+}
+
+console.log(num1);
