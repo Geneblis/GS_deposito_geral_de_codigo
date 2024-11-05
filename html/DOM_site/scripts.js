617af9282d519b71c3d8ea17e7d0188fb3203bff
Geneblis <94307472+Geneblis@users.noreply.github.com>
2024-11-05 13:58:43 +0000

    No need for abstraction to print tfucking Hello World bruh.

---
+++ html/DOM_site/scripts.js
@@ -0,0 +1,25 @@
+let cabecalho = document.getElementById("meu-txt")
+let quadrado = document.getElementById("quadrado")
+
+cabecalho.innerText = "Texto Atualizado"
+cabecalho.style.color = "blue"
+cabecalho.style.fontSize = "40px"
+cabecalho.style.fontFamily = "Arial"
+cabecalho.style.backgroundColor = "red"
+
+function mudarCor(){
+    quadrado.style.backgroundColor = "green"
+}
+
+function desfazerCor(){//bruh
+    quadrado.style.backgroundColor = "cyan"
+}
+
+function diminuirTamanho(){
+    quadrado.style.height = "100px"
+    quadrado.style.width = "100px"
+}
+function defaultTamanho(){
+    quadrado.style.height = "500px"
+    quadrado.style.width = "500px"
+}
