60beeea32d2b4a82bf1e512928aa052bfee80e34
Geneblis <94307472+Geneblis@users.noreply.github.com>
2024-11-08 22:59:10 -0300

    arrumando codigos cagados da prof

--- html/DOM_site/scripts.js
+++ html/DOM_site/scripts.js
@@ -1,25 +1,32 @@
-let cabecalho = document.getElementById("meu-txt")
-let quadrado = document.getElementById("quadrado")
+document.addEventListener("DOMContentLoaded", function() {
+    let cabecalho = document.getElementById("meu-txt")
+    let quadrado = document.getElementById("quadrado")
 
-cabecalho.innerText = "Texto Atualizado"
-cabecalho.style.color = "blue"
-cabecalho.style.fontSize = "40px"
-cabecalho.style.fontFamily = "Arial"
-cabecalho.style.backgroundColor = "red"
+    cabecalho.innerText = "Texto Atualizado"
+    cabecalho.style.color = "blue"
+    cabecalho.style.fontSize = "40px"
+    cabecalho.style.fontFamily = "Arial"
+    cabecalho.style.backgroundColor = "red"
 
-function mudarCor(){
-    quadrado.style.backgroundColor = "green"
-}
+    function mudarCor(){
+        quadrado.style.backgroundColor = "green"
+    }
 
-function desfazerCor(){//bruh
-    quadrado.style.backgroundColor = "cyan"
-}
+    function desfazerCor(){//bruh
+        quadrado.style.backgroundColor = "cyan"
+    }
 
-function diminuirTamanho(){
-    quadrado.style.height = "100px"
-    quadrado.style.width = "100px"
-}
-function defaultTamanho(){
-    quadrado.style.height = "500px"
-    quadrado.style.width = "500px"
-}
+    function diminuirTamanho(){
+        quadrado.style.height = "100px"
+        quadrado.style.width = "100px"
+    }
+    function defaultTamanho(){
+        quadrado.style.height = "500px"
+        quadrado.style.width = "500px"
+    }
+
+    window.mudarCor = mudarCor;
+    window.desfazerCor = desfazerCor;
+    window.diminuirTamanho = diminuirTamanho;
+    window.defaultTamanho = defaultTamanho;
+});
