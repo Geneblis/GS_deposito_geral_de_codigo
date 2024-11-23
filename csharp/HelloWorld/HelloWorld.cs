6cd532b9dc54a525e3f85549bec49921685bfbc5
Geneblis <94307472+Geneblis@users.noreply.github.com>
2024-11-23 08:10:17 -0300

    csharp sem putaria (use mono)

---
+++ csharp/HelloWorld/HelloWorld.cs
@@ -0,0 +1,13 @@
+//pra compilar abra a pasta no terminal e coloque:
+//mcs "nome do arquivo".cs
+//mono "nomedoarquivo".exe
+
+using System;
+
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
