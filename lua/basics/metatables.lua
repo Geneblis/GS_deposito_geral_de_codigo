452c2a84dfe3488c0dbb63c41c51e40bf1f07982
Geneblis <94307472+Geneblis@users.noreply.github.com>
2025-05-13 16:32:08 -0300

    Update metatables.lua

--- lua/basics/metatables.lua
+++ lua/basics/metatables.lua
@@ -7,3 +7,18 @@ end
 -- aplica a metatable a T
 setmetatable(T, mt)
 print(T)
+
+-------------------------------------------------------------------
+local mapTable = {Mon="Monday", Tue="Tuesday", Wed="Wednesday"}
+-- set the metatable
+setmetatable(mapTable, {
+   -- self represent the table on which metatable is applied
+   -- index is the non-existing index
+   __index = function(self, index)
+    return "Not defined."
+   end
+})
+-- print value using existing key
+print(mapTable.Mon)
+-- print value using non-exiting key
+print(mapTable.Sun)
